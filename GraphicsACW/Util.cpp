de6cd69f2965420e20397ebb4ed3992e8a7d5978
David Hart <david.hart555@googlemail.com>
2011-10-26 19:45:47 +0000

    Further simplified ReadTextFileToString

--- GraphicsACW/Util.cpp
+++ GraphicsACW/Util.cpp
@@ -8,7 +8,5 @@ void Util::ReadTextFileToString(const std::string& filename, std::string& text)
 	std::ifstream file(filename);
 	assert(file.is_open());
 
-	std::string temp((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-
-	text.swap(temp);
+	text.assign((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 }
